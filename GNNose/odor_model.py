5500f8aec3cf2d3d642589b7fbb928b942c995a4
Cathy Zhou <cathyzbn@stanford.edu>
2023-03-22 17:09:00 -0400

    xgboost

--- GNNose/odor_model.py
+++ GNNose/odor_model.py
@@ -5,11 +5,14 @@ import torch_geometric.nn as pyg_nn
 import torch_geometric.utils as pyg_utils
 from torch_geometric.nn.models import GAT, GIN
 from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool
+from xgboost import XGBClassifier
 
 class GraphClassifier(torch.nn.Module):
     def __init__(self, in_channels, out_channels, pooling_type='mean'):
         super().__init__()
         self.mlp = torch.nn.Linear(in_channels, out_channels)
+        self.clf = XGBClassifier(tree_method='gpu_hist',
+                             num_class = 6,objective = 'multi:softprob')
         if pooling_type == 'mean':
           self.pool = global_mean_pool
         elif pooling_type == 'max':
@@ -19,6 +22,9 @@ class GraphClassifier(torch.nn.Module):
 
     def forward(self, x, batch):
         x = self.pool(x, batch)
+        preds = self.clf.predict(x.detach().cpu().numpy())
+        preds = torch.tensor(preds)
+        return preds
         return torch.sigmoid(self.mlp(x))
 
     def reset_parameters(self):
